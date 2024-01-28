2b127038b1e5badfdc16e9766fa70cac2b4b2f0f
Will Zhang <wz282@cornell.edu>
2024-01-28 16:25:25 -0500

    set configs for npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/lib/main.js'),
+      name: 'Shmood DnD Board',
+      fileName: 'shmood-dnd-board',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+
+    },
+  },
   plugins: [react()],
 })
